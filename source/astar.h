b066846fc484ab3658b9c1723b799537f7d55232
TheRealMattHUN <legolegend007@gmail.com>
2024-03-28 20:44:24 +0100

    Major performance improvement with new set implementation. Plus fixed some errors and typos.

--- source/astar.h
+++ source/astar.h
@@ -5,6 +5,7 @@
 #define PATHFINDING_ASTAR_ASTAR_H
 #endif //PATHFINDING_ASTAR_ASTAR_H
 #include <unordered_map>
+#include <set>
 #include <vector>
 namespace A_star {
     // Coordinate with an x and y value
@@ -21,12 +22,6 @@ namespace A_star {
             return {left_.x + right_.x, left_.y + right_.y};
         }
     };
-    // Special hashing algo for unordered_map to store values with a Coordinate key
-    struct coord_hash {
-        size_t operator() (const Coordinate& coords_) const {
-            return ((size_t)coords_.x << 16) | coords_.y;
-        }
-    };
     // Hard-coded directions, since we can add Coordinates together now
     Coordinate dir[8] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
     // Node
@@ -41,8 +36,26 @@ namespace A_star {
         // Constructor
         Node(Coordinate coords_, Node* parent_, unsigned int g_, unsigned int h_) : coords(coords_), parent(parent_), g(g_), h(h_) {};
     };
-    // Type definition for unordered_map
+    // Special hashing algo for unordered_map to store values with a Coordinate key
+    struct coord_hash {
+        size_t operator() (const Coordinate& coords_) const {
+            return ((size_t)coords_.x << 32) | coords_.y;
+        }
+    };
+    // Special sorting algo for the open set to store Node pointers by H value
+    // I'm using the H value instead of the F value because
+    // I don't see a point why to do so when so many nodes share the F value.
+    struct sortbyHValue {
+        bool operator() (const Node* left_, const Node* right_) const {
+            return left_->h < right_->h;
+        }
+    };
+    // Type definition for unordered_map (for the "closed" map and the "walls" map)
     typedef std::unordered_map<Coordinate, Node*, coord_hash> nodeMap;
+    // Type definition for set (for the open set)
+    typedef std::set<Node*, sortbyHValue> nodeSet;
+    // Type definition for unordered_map (for the "open set iterator" map)
+    typedef std::unordered_map<Coordinate, nodeSet::iterator, coord_hash> nodeSetMap;
     // Admissible heuristics (ones that don't overestimate the cost of travelling from current node to goal node)
     class Heuristic {
     public:
@@ -51,10 +64,20 @@ namespace A_star {
             unsigned int dx = abs(start_.x - goal_.x), dy = abs(start_.y - goal_.y);
             return 10 * (dx + dy);
         }
-        // Diagonal heuristic for grids that only allow 4 directions of movement
+        // Diagonal heuristic for grids that only allow 8 directions of movement
         static unsigned int diagonal(const Coordinate& start_, const Coordinate& goal_) {
             unsigned int dx = abs(start_.x - goal_.x), dy = abs(start_.y - goal_.y);
             return 10 * (dx + dy) - (6 * std::min(dx, dy));
+        }
+        // Euclidean heuristic that allows for more than 8 directions
+        static unsigned int euclidean(const Coordinate& start_, const Coordinate& goal_) {
+            unsigned int dx = abs(start_.x - goal_.x), dy = abs(start_.y - goal_.y);
+            // Getting the answer with square root approximation (7 iterations are enough, it seems)
+            // Starting off with the sum of dx2 + dy2
+            unsigned int num = (dx*dx + dy*dy), estimate = 1;
+            // Heron's method
+            for (unsigned short i = 0; i < 7; i++) estimate = 0.5 * (estimate + num/estimate);
+            return estimate;
         }
     };
     // Map
@@ -70,6 +93,10 @@ namespace A_star {
         void addWall(const Coordinate& wall_) {
             walls[wall_] = new Node(wall_, nullptr, 0, 0);
         }
+        // Clear walls
+        void clearWalls() {
+            walls.clear();
+        }
         // Is coordinate valid? (is inside boundaries and isn't a wall)
         bool isValid(const Coordinate& coords_) const {
             return (coords_.x > -1 && coords_.y > -1 && coords_.x < mapSize_x && coords_.y < mapSize_y && walls.find(coords_) == walls.end());
@@ -78,9 +105,9 @@ namespace A_star {
     // Searching algo and its supplements
     class Search {
     private:
-        //Reconstruct
-        //We iterate through the parents of the nodes from the final node, since that is the shortest path
-        //We add them to a vector of Coordinate, and that will be our final result.
+        // Reconstruct
+        // We iterate through the parents of the nodes from the final node, since that is the shortest path
+        // We add them to a vector of Coordinate, and that will be our final result.
         static std::vector<Coordinate> reconstruct(Node* current) {
             std::vector<Coordinate> result;
             result.push_back(current->coords);
@@ -90,50 +117,62 @@ namespace A_star {
             }
             return result;
         }
-        //lowestHValue
-        //We iterate through the open map to find the Node pointer that has the minimum H value in the map
-        //Every pseudocode of A-star ever says that you must search for the lowest F, but I don't see the point in doing that when you already
-        //precomputed H, so I'm doing this instead.
-        static nodeMap::iterator lowestHValue(nodeMap& open_) {
-            auto result = open_.begin();
-            for (auto it = open_.begin(); it != open_.end(); it++) {
-                if (it->second->h < result->second->h) result = it;
-            }
-            return result;
-        }
+        // We iterate through the open map to find the Node pointer that has the minimum H value in the map
+        // Every pseudocode of A-star ever says that you must search for the lowest F, but I don't see the point in doing that,
+        // since a lot of other nodes have the same F values, so I'm doing this instead.
     public:
-        //findPath, the main algo
+        // findPath, the main algo
+        // Returns
+        //      -> an empty vector if no path exists from start node to goal node
+        //      -> every coordinate on the path, including the start node and the goal node
         static std::vector<Coordinate> findPath(const Coordinate& start_, const Coordinate& goal_, const bool& diagonal_, const Map& map) {
-            if (map.mapSize_x <= 0  || map.mapSize_y <= 0) return {};
-            if (!map.isValid(start_) || !map.isValid(start_)) return {};
-            nodeMap closed, open;
-            open[start_] = new Node(start_, nullptr, 0, diagonal_ ? Heuristic::diagonal(start_, goal_) : Heuristic::manhattan(start_, goal_));
+            // If start node or goal node is invalid, return an empty vector
+            if (!map.isValid(start_) || !map.isValid(start_) || map.mapSize_x <= 0  || map.mapSize_y <= 0) return {};
+            // Closed map
+            nodeMap closed;
+            // Open set (sorted by H value)
+            nodeSet open;
+            // A Coordinate map for storing iterators for the open set (for access with the Coordinate type)
+            nodeSetMap itrMap;
+            // Put the starting node in the open set and the Coordinate map
+            itrMap[start_] = open.insert(new Node(start_, nullptr, 0, diagonal_ ? Heuristic::diagonal(start_, goal_) : Heuristic::manhattan(start_, goal_))).first;
+            // Prepping a bunch of stuff
             Node* current;
             Coordinate next;
-            nodeMap::iterator it;
             short directions = (diagonal_) ? 8 : 4;
             unsigned int tentative;
+            // Now the fun begins...
             while (!open.empty()) {
-                it = lowestHValue(open);
-                current = it->second;
-                if (current->coords == goal_) return reconstruct(current);
+                // Get an iterator for the Node with the lowest H value
+                current = *open.begin();
+                // If it's the goal, return the path
+                if (current->h == 0) return reconstruct(current);
+                // Else put this Node in the closed map, and erase it from the set and other map
                 closed[current->coords] = current;
-                open.erase(it);
+                itrMap.erase(itrMap.find(current->coords));
+                open.erase(open.begin());
+                // For all neighbors of this Node
                 for (short i = 0; i < directions; i++) {
+                    // Get Coordinate
                     next = current->coords + dir[i];
+                    // If Coordinate is invalid (is a wall or out of boundaries), or is already evaluated (in the closed map), skip this neighbor
                     if (!map.isValid(next) || closed.find(next) != closed.end()) {
                         continue;
                     }
+                    // Calculate the G cost of this neighbor
                     tentative = current->g + ((i < 4) ? 10 : 14);
-                    if (open.find(next) == open.end()) {
-                        open[next] = new Node(next, current, tentative, diagonal_ ? Heuristic::diagonal(next, goal_) : Heuristic::manhattan(next, goal_));
+                    // If this neighbor isn't in the open set and Coordinate map, insert it
+                    if (itrMap.find(next) == itrMap.end()) {
+                        itrMap[next] = open.insert(new Node(next, current, tentative, diagonal_ ? Heuristic::diagonal(next, goal_) : Heuristic::manhattan(next, goal_))).first;
                     }
-                    else if (tentative < open[next]->g) {
-                        open[next]->parent = current;
-                        open[next]->g = tentative;
+                    // Else if it has a lower G value than what is stored, update its G cost and parent
+                    else if (tentative < (*itrMap[next])->g) {
+                        (*itrMap[next])->g = tentative;
+                        (*itrMap[next])->parent = current;
                     }
                 }
             }
+            // If we checked every possibility, return an empty vector
             return {};
         }
     };
